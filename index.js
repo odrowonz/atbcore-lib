21721bd7502762fe11818994ed7c3c6993a8341c
segwit <segwitcoin@gmail.com>
2017-09-14 12:28:27 +0300

    Remove Exception if More than one instance of atbcore-lib found.

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ var bitcore = module.exports;
 bitcore.version = 'v' + require('./package.json').version;
 bitcore.versionGuard = function(version) {
   if (version !== undefined) {
-    var message = 'More than one instance of bitcore-lib found. ' + 
-      'Please make sure to require bitcore-lib and check that submodules do' +
-      ' not also include their own bitcore-lib dependency.';
-    throw new Error(message);
+    var message = 'More than one instance of atbcore-lib found. ' +
+      'Please make sure to require atbcore-lib and check that submodules do' +
+      ' not also include their own atbcore-lib dependency.';
+      console.warn(message, version);
   }
 };
 bitcore.versionGuard(global._atbcore);
